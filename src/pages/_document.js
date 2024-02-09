56815182d85f8846372596f4626da3cda9166570
Ryan Valle <ryanvalle@me.com>
2024-02-08 21:33:51 -0800

    basic analytics

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -5,7 +5,7 @@ export default function Document() {
     <Html lang="en">
       <Head />
       <body>
-      {/* <Script strategy="afterInteractive" src="https://www.googletagmanager.com/gtag/js?id=G-HLFDYBLJMB"/>
+      <Script strategy="afterInteractive" src="https://www.googletagmanager.com/gtag/js?id=G-33W1P1HC7H"/>
       <Script
           id='google-analytics'
           strategy="afterInteractive"
@@ -14,12 +14,10 @@ export default function Document() {
                   window.dataLayer = window.dataLayer || [];
                   function gtag(){dataLayer.push(arguments);}
                   gtag('js', new Date());
-                  gtag('config', 'G-HLFDYBLJMB', {
-                      page_path: window.location.pathname,
-                  });
+                  gtag('config', 'G-33W1P1HC7H');
               `,
           }}
-      /> */}
+      />
         <Main />
         <NextScript />
       </body>
